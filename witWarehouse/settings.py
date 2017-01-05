c0e4e2c2e908065b5caeb58bc763583414b587f8
lin <leo0102@hotmail.com>
2017-01-05 11:57:01 +0800

    提交react  jQuery库 增加静态资源加在配置

--- witWarehouse/settings.py
+++ witWarehouse/settings.py
@@ -113,13 +113,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATIC_ROOT = os.path.join(os.path.dirname(__file__),'static')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'witWarehouse/../templates'),
     'wit',
 )
+
