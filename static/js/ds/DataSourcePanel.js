b12e629e544bf922c585fb384203cfd1179a39c9
lin <leo0102@hotmail.com>
2017-01-18 20:59:02 +0800

    实现数据源卡片布局控件，下一步需要把工作放到ETL数据接入，带数据接入后，再回到这里，做数据集成。

--- static/js/ds/DataSourcePanel.js
+++ static/js/ds/DataSourcePanel.js
@@ -6,15 +6,40 @@ import ReactDOM from 'react-dom';
 
 export default class DataSourcePanel extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
+        this.onSearchChange = this.onSearchChange.bind(this);
+        this.state = {value: ''};
     }
 
     componentWillMount() {
     }
 
+    onSearchChange(value) {
+        console.log(value);
+        this.setState({value: value});
+    }
+
     render() {
+        const value = this.state.value;
         return <div key="dataSourcePanel">
-            <SearchBar/>
+            <SearchBar value={value} onChange={this.onSearchChange}/>
+
+            <div className="row">
+                <div className="col-xs-6 col-sm-3">
+                    <div className="panel panel-default">
+                        <div className="panel-body">
+                            Basic panel example
+                        </div>
+                    </div>
+                </div>
+                <div className="col-xs-6 col-sm-3">
+                    <div className="panel panel-default">
+                        <div className="panel-body">
+                            Basic panel example
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>;
     }
 }
@@ -23,11 +48,20 @@ export default class DataSourcePanel extends React.Component {
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.props.onChange(e.target.value);
     }
 
     render() {
+        const value = this.props.value;
         return <div className="row">
-
-        </div>;
+                <div className="form-group">
+                    <input type="text" value={value} onChange={this.handleChange} className="form-control" id="filter"/>
+                </div>
+            </div>;
     }
 }
+
